f9b2ff9a54b5787affb708bcd3adac8d3850cacf
Nobuna <nicolas.brion2@etu.univ-lorraine.fr>
2022-10-27 11:38:52 +0200

    Initial Commit

---
+++ pages/layout/header.js
@@ -0,0 +1,35 @@
+import styles from "/styles/header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <button className={styles.button}>
+                <svg viewBox="0 0 24 24">
+                <path d="M3 12H15" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                <path d="M3 6H21" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                <path d="M3 18H9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </button>
+            <form className={styles.Search}>
+                <input className={styles.Search_input} placeholder="Search" name="content"/>
+                <svg className={styles.Lens}viewBox="0 0 24 24" title="Lens" fill="none">
+                    <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M21 20.9999L16.65 16.6499" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </form>
+            <div className={styles.open_profile} id="Open_Profile" title="Avatar">
+                <svg viewBox="0 0 24 24" fill="none">
+                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2m8-10a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+            <div className={styles.close_profile} title="Close_Profile">
+                <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M18 6L6 18" />
+                    <path d="M6 6L18 18" />
+                </svg>
+            </div>
+        </header>
+    )
+}
